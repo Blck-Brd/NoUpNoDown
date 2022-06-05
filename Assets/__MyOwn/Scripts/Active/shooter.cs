ed57984a1b4cec8e45c9722868a7b33fc7ddc1e2
BlckBrd <andrej.kos.cobain@gmail.com>
2022-06-05 20:48:04 +0200

    working on shooter script to be able to use animLayers

---
+++ Assets/__MyOwn/Scripts/Active/shooter.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooter : MonoBehaviour
+{
+    public bool gunRaised;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+   
+    }
+
+
+void HandleControls()
+{
+if(Input.GetKeyDown(KeyCode.))
+}
+
+
+}
